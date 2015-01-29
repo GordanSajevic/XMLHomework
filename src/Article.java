8a34a4591c009afa482f96531f33626e537520df
Gordan Sajević <gsajevic@hotmail.com>
2015-01-29 07:47:09 +0100

    initial commit

---
+++ src/Article.java
@@ -0,0 +1,52 @@
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Article {
+	
+	private String title;
+	private String content;
+	private LinkedList<Article> articles;
+	
+	public Article(String title, String content) 
+	{
+		this.articles = new LinkedList<Article>();
+		this.title = title;
+		this.content = content;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		if (title.length()<1)
+		{
+			throw new IllegalArgumentException("Title cannot be empty field!");
+		}
+		else
+		{
+			this.title = title;
+		}
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		if (content.length() < 1)
+		{
+			throw new IllegalArgumentException("Content cannot be empty field!");
+		}
+		else
+		{
+			this.content = content;
+		}
+	}
+
+}
---
+++ src/XMLReader.java
@@ -0,0 +1,85 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+public class XMLReader {
+	
+	public static void main(String[] args) throws SAXException, IOException {
+		try {
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter number of article: ");
+			int num = input.nextInt();
+			URL url = new URL("http://www.klix.ba/rss/svevijesti");
+			URLConnection connection = url.openConnection();
+			DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			String homeDir = System.getProperty("user.home");
+			String path = homeDir + File.separator + "workspace" + File.separator + 
+					 "XMLHomework" + File.separator + "Folder" + File.separator + 
+					 File.separatorChar + "klix.xml";
+			Document xmlDoc = db.parse(connection.getInputStream());
+			NodeList xmlArticles = xmlDoc.getElementsByTagName("item");
+			LinkedList<Article> articles = new LinkedList<Article>();
+			Article ar = null;
+			String childTitle = null;
+			String childArticle = null;
+			for (int i=0; i<xmlArticles.getLength(); i++)
+			{
+				Node current = xmlArticles.item(i);
+				if (current instanceof Element)
+				{
+					NodeList xmlChildren = current.getChildNodes();
+					for (int j=0; j<xmlChildren.getLength(); j++)
+					{
+						Node currentChild = xmlChildren.item(j);
+						if (currentChild instanceof Element)
+						{
+							Element currentChildElement = (Element) currentChild;
+							if (currentChildElement.getTagName().equals("title")) {
+								childTitle = currentChildElement.getTextContent();
+							}
+							if (currentChildElement.getTagName().equals("clanak")) {
+								childArticle = currentChildElement.getTextContent();
+							}
+							ar = new Article(childTitle, childArticle);
+						}
+					}
+					articles.add(ar);
+				}
+			}
+			Iterator<Article> it = articles.iterator();
+			int counter = 0;
+			while (it.hasNext()) 
+			{
+				counter++;
+
+				Article art = it.next();
+				if (counter == num) 
+				{
+					System.out.println("CLANAK " + counter + "\n" + art.toString() + "\n\n");
+					break;
+				}
+			}
+			
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
